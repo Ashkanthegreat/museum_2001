0efbd2b07dc984c9507ce2cbc3515de4a9682cd3
Ashkan Abbasi <airashkan@yahoo.com>
2020-02-19 09:23:37 -0700

    Create Exhibit test it exists

---
+++ test/exhibit_test.rb
@@ -0,0 +1,19 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/exhibit'
+require './lib/patron'
+
+
+class ExibitTest < Minitest::Test
+
+  def setup
+    @exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+  end
+
+
+  def test_it_exists
+    assert_instance_of Exhibit, @exhibit
+  end
+
+
+end
