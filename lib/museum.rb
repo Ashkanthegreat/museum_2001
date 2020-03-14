c3954f0319191ccce9c9111d2292f2fa7081ab65
Ashkan Abbasi <airashkan@yahoo.com>
2020-03-13 22:45:10 -0600

    Add patrons by exhibit interest test and method

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,9 +1,10 @@
 class Museum
-  attr_reader :name, :exhibits
+  attr_reader :name, :exhibits, :patrons
 
   def initialize(name)
     @name = name
     @exhibits = []
+    @patrons = []
   end
 
   def add_exhibit(exhibit)
@@ -11,13 +12,22 @@ class Museum
   end
 
   def recommend_exhibits(patron)
-    exhibits.find_all do |exhibit|
-      patron.interests.include?(exhibit.name)
-    end
+    exhibits.find_all {|exhibit|
+      patron.interests.include?(exhibit.name)}
   end
 
-  def add_patrons
-    
+  def admit(patron)
+    @patrons << patron
+  end
+
+  def patrons_by_exhibit_interest
+    @exhibits.reduce({}) do |exhibit_patrons, exhibit|
+      exhibit_patrons[exhibit] = @patrons.find_all do |patron|
+        patron.interests.include?(exhibit.name)
+      end
+      exhibit_patrons
+    end
+  end
 
 
 end
